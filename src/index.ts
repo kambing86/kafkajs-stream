b78ad5d7301f75806225d0a4c9fce0ddd7104e6b
Chua Kang Ming <kambing860210@gmail.com>
2019-12-20 14:51:27 +0800

    fix: export object stream

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,4 @@
+export { ConsumerObjectStream } from './ConsumerObjectStream';
 export { ConsumerStream } from './ConsumerStream';
+export { ProducerObjectStream } from './ProducerObjectStream';
 export { ProducerStream } from './ProducerStream';
